02e1c7f67a3ed1c60a3daf2776d72893ad85ac80
Joseph Mauricio Mondragon Moreno <jmauriciomondragonm@gmail.com>
2026-05-20 12:38:15 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_2_Equipo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
